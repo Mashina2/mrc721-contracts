0c97a73f79eb7c487d06c9072da402dc23327fd7
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-01-26 13:27:19 +0330

    first commit

---
+++ contracts/MRC721.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
+contract MRC721 is ERC721Enumerable, AccessControl{
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    string public _baseTokenURI;
+
+    constructor(
+        string memory _name, 
+        string memory _symbol,
+        string memory _uri
+    ) ERC721(_name, _symbol){
+        _baseTokenURI = _uri;
+    }
+
+
+    function mint(address to, uint256 id) external onlyRole(MINTER_ROLE){
+        _mint(to, id);
+    }
+
+    function burn(uint256 tokenId) public{
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "caller is not owner nor approved");
+        _burn(tokenId);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721Enumerable, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,31 @@
+var bridge = artifacts.require('./MuonNFTBridge.sol')
+
+const pubKeyAddress = process.env.MUON_MASTER_WALLET_PUB_ADDRESS;
+const pubKeyX = process.env.MUON_MASTER_WALLET_PUB_X;
+const pubKeyYParity = process.env.MUON_MASTER_WALLET_PUB_Y_PARITY;
+
+function parseArgv(){
+	let args = process.argv.slice(2);
+	let params = args.filter(arg => arg.startsWith('--'))
+	let result = {}
+	params.map(p => {
+		let [key, value] = p.split('=');
+		result[key.slice(2)] = value === undefined ? true : value
+	})
+	return result;
+}
+
+module.exports = function (deployer) {
+	deployer.then(async () => {
+		let {muonAddress} = parseArgv()
+
+		let  = null;
+
+		if(!muonAddress){
+			throw {message: "muonAddress required."}
+		}
+
+		console.log(`deploying MuonV02 muonAddress=${muonAddress} ...`)
+		let deployedBridge = await deployer.deploy(bridge, muonAddress)
+	})
+}
