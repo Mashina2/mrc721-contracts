5e311172c5a6854b9675fdf861e17d3298c4cdbd
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-02-07 12:41:24 +0330

    bugfix

--- contracts/MetaMartianNFT.sol
+++ contracts/MetaMartianNFT.sol
@@ -12,5 +12,6 @@ contract MetaMartianNFT is MRC721{
         "https://mmac-meta-martian.communitynftproject.io/"
     ){
     	_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    	_setupRole(MINTER_ROLE, msg.sender);
     }
 }
